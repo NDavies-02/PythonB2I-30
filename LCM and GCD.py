15b94352e1f22c390740aa67e05f83a5c4d94505
Nathan Davies <29925490+NDavies-02@users.noreply.github.com>
2021-07-18 21:14:32 +0100

    Coding Exercise 1 Initial Solution

---
+++ LCM and GCD.py
@@ -0,0 +1,35 @@
+import math
+
+def usrinput():
+    a=int(input("Please enter the first number: "))
+    b=int(input("Please enter the second number: "))
+    return (a,b)
+    
+def selection(num1, num2):
+    print("Please make a selection.")
+    choice=input("1 - Calculate GCD; 2 - Calculate LCM; 3 - Help: ")
+    if choice=="1":
+        calcGCD(num1,num2)
+    elif choice=="2":
+        calcLCM(num1,num2)
+    elif choice=="3":
+        print("This simple program allows you to enter 2 numbers.")
+        print("You can then choose to calculate the GCD or LCM of those numbers.")
+        print("GCD = Greatest Common Denominator")
+        print("LCM = Lowest Common Multiple")
+        selection()
+
+def calcGCD(a, b):
+    result=math.gcd(a,b)
+    result=str(result)
+    print("The GCD is: " + result)
+
+def calcLCM(a, b):
+    result=math.lcm(a,b)
+    result=str(result)
+    print("The LCM is: " + result)
+
+numbers=usrinput()
+num1=numbers[0]
+num2=numbers[1]
+selection(num1,num2)
